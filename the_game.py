5c68b89544036e7c33e46d5dfadac94f6e1ef063
raymond <nvkienn@raymonds-MacBook-Air.local>
2022-06-07 16:32:43 +0800

    finalising wordle game

--- the_game.py
+++ the_game.py
@@ -7,4 +7,9 @@ for i in range (6):
         guess = input ("Invalid, enter guess:")
     outcome = colorize (guess,ans)
     print (outcome)
+    if (guess==ans):
+        print ('YOU GUESSED IT')
+        break
+if (guess!=ans):
+    print ('You failed.')
 
