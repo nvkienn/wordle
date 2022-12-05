89fd192f92542834f2066480b98ab79c8dd28483
nguyenvukhang <brew4k@gmail.com>
2022-12-05 11:39:37 +0800

    add comments to loader

--- java/Loader.java
+++ java/Loader.java
@@ -1,11 +1,30 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 
+/**
+ * Loads a directory containing data files into a two layer dictionary.
+ * {
+ *   [ans]: {
+ *     [guess]: "outcome",
+ *     ...
+ *   },
+ *   ...
+ * }
+ */
 public class Loader {
-    static HashMap<String, String> readFile(File file) {
+    /**
+     * Reads a file containing 10-character lines.
+     * Filename is the answer.
+     * First 5 characters is the guess.
+     * Last 5 characters is the outcome of taking that guess.
+     */
+    private static HashMap<String, String> readFile(File file) {
         HashMap<String, String> data = new HashMap<String, String>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -15,31 +34,37 @@ public class Loader {
                 data.put(guess, outcome);
             });
             reader.close();
-        } catch (Exception e) {
-            System.err.printf("File not found: %s\n", file);
+        } catch (FileNotFoundException e) {
+            System.out.printf("File not found: %s\n", file);
+        } catch (IOException e) {
+            System.out.printf("Error reading file: %s\n", file);
         }
         return data;
     }
 
-    static File[] getDataFiles() {
-        File dataFolder =
-            Paths.get("").toAbsolutePath().getParent().resolve("data").toFile();
-        return dataFolder.listFiles();
+    /**
+     * Gets the path to the data file directory.
+     * hard-coded to take ../data/ relative to this java code file.
+     */
+    private static Path getDataDir() {
+        Path currentDir = Paths.get("").toAbsolutePath();
+        return currentDir.getParent().resolve("data");
     }
 
     static HashMap<String, HashMap<String, String>> loadDatabase() {
-        File[] dataFiles = getDataFiles();
-        HashMap<String, HashMap<String, String>> db =
+        File[] dataFiles = getDataDir().toFile().listFiles();
+        HashMap<String, HashMap<String, String>> database =
             new HashMap<String, HashMap<String, String>>(dataFiles.length);
+        System.out.printf("Reading %d files", dataFiles.length);
         for (int i = 0; i < dataFiles.length; i++) {
-            if (i % 200 == 0) {
-                System.err.printf("read file: %d/%d\n", i, dataFiles.length);
-            }
+            if (i % 200 == 0)
+                System.out.print('.');
             File dataFile = dataFiles[i];
             String ans = dataFile.getName();
             HashMap<String, String> ansDict = readFile(dataFiles[i]);
-            db.put(ans, ansDict);
+            database.put(ans, ansDict);
         }
-        return db;
+        System.out.print("done!\n");
+        return database;
     }
 }
