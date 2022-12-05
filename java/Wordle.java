92b5e3cd1fd43d850779eeb6e6cc052fef313d0d
nguyenvukhang <brew4k@gmail.com>
2022-12-05 11:49:22 +0800

    add comments to wordle

--- java/Wordle.java
+++ java/Wordle.java
@@ -1,23 +1,18 @@
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 public class Wordle {
     private static final String FIRST_GUESS = "soare";
     final Database db;
-    final Stats stats = new Stats("stats.txt");
     final AnsList ansList;
     double totalGuesses = 0.0;
     double totalNanoTime = 0.0;
     int done = 0;
     final String firstGuess;
-    final PrintWriter io;
 
     Wordle() {
         this.db = new Database(Loader.loadDatabase());
         this.ansList = new AnsList(db);
-        this.io = new PrintWriter(System.out);
         if (FIRST_GUESS.isEmpty()) {
             System.out.println("generating first guess...");
             this.firstGuess = db.nextGuess(ansList);
@@ -45,7 +40,6 @@ public class Wordle {
             }
             String outcome = db.getOutcome(ans, guess);
             path.add(outcome);
-            stats.recordGuess(guess, turn);
             ansList.filter(guess, outcome);
         }
 
@@ -56,27 +50,24 @@ public class Wordle {
         // timer stuff
         long endTime = System.nanoTime();
         this.totalNanoTime += endTime - startTime;
-
-        io.printf("`%s` in %d tries.\n", ans, turn);
+        System.out.printf("`%s` in %d tries.\n", ans, turn);
         return turn;
     }
 
-    int interval = 10;
-
     void display(int numAns) {
-        io.printf("[%d/%d] avg.guess = %.3f\n", done, numAns,
-                  totalGuesses / done);
-        double avgTime = this.totalNanoTime / done / 1000000;
-        io.printf("avg time = %.3fms\n", avgTime);
+        double avgGuess = totalGuesses / done;
+        double avgTime = totalNanoTime / done / 1000000;
+        System.out.printf("[%d/%d] avg.guess = %.3f\n", done, numAns, avgGuess);
+        System.out.printf("avg time = %.3fms\n", avgTime);
+
         double remainingInSeconds = avgTime * (numAns - done) / 1000;
         int seconds = (int)remainingInSeconds % 60;
         int minutes = (int)remainingInSeconds / 60;
-        io.printf("time remaining: %d min %d sec\n", minutes, seconds);
-        if (done % interval == 0)
-            io.flush();
+        System.out.printf("time remaining: %d min %d sec\n", minutes, seconds);
     }
 
     /**
+     * Main loop for auto-solver.
      * Solves against every possible answer and tracks results.
      */
     void auto() {
@@ -89,49 +80,29 @@ public class Wordle {
             display(numAns);
         }
         timer.end();
-        io.flush();
-        this.stats.saveToFile();
         timer.print("Wordle::auto()");
-        this.stats.print();
-        io.close();
     }
 
-    /**
-     * Solves against queried answers.
-     */
-    void auto(List<String> solveList) {
-        List<String> allPossibleAns = this.db.answers;
-        int numAns = allPossibleAns.size();
-        for (String ans : solveList) {
-            this.totalGuesses += solve(ans);
-            if (done++ % 100 == 0)
-                this.stats.saveToFile();
-            display(numAns);
+    static void help(String[] commands) {
+        System.err.println("Run with one of these subcommands:");
+        for (String command : commands) {
+            System.err.printf("  * %s\n", command);
         }
     }
 
-    /**
-     * For running any benchmarks.
-     */
-    void bench() {
-        Timer timer = Timer.start();
-        timer.end();
-        timer.print("That");
-    }
-
     public static void main(String[] args) {
-        if (args.length < 1)
-            System.err.println("Run with `auto` or `bench` as argument.");
-        Wordle wordle = new Wordle();
+        String[] commands = new String[] {"auto"};
+        if (args.length < 1) {
+            help(commands);
+            return;
+        }
         switch (args[0]) {
             case "auto":
+                Wordle wordle = new Wordle();
                 wordle.auto();
                 break;
-            case "bench":
-                wordle.bench();
-                break;
             default:
-                System.err.println("Run with `auto` or `bench` as argument.");
+                help(commands);
         }
     }
 }
