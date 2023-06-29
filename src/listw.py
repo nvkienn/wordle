913807b1a2ea5f285a662010e8f97d431ada3005
nguyenvukhang <brew4k@gmail.com>
2023-06-29 12:34:09 +0800

    feat: resolve all paths

--- src/listw.py
+++
@@ -1,9 +0,0 @@
-def lines(file):
-    data = []
-    with open(file) as f:
-        data = f.read().splitlines()
-    return data
-
-
-answers = lines("./words/answers.txt")
-guesses = lines("./words/guesses.txt")
---
+++ src/wordle/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import solver
+# import game
+# import external_solver
+
+import sys
+
+
+def main():
+    print(sys.argv)
